33e15c28155481458967a003e449bf22b3a3c1c6
kkuleshov92 <nstkuleshov1@gmail.com>
2019-07-25 18:03:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function tabsAction(parentClass, tabsClass, tabsContentClass) {
+    
+    let tab = document.querySelectorAll('.' + tabsClass),
+        info = document.querySelector('.' + parentClass),
+        tabContent = document.querySelectorAll('.' + tabsContentClass);
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+
+        if (target && target.classList.contains(tabsClass)) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        };
+    });
+}
